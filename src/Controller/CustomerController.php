34b6502b3a747625fccb1aed67b3488c23c6115f
lostredakos <e.giannakosian@gmail.com>
2019-02-02 21:07:56 +0200

    added bootstrap css and js

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -9,18 +9,29 @@
 namespace App\Controller;
 
 
+
+use App\Entity\Account;
+use App\Entity\User;
+use http\Env\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CustomerController extends AbstractController
 {
     /**
-     * @Route("/customers", name="customers")
+     * @Route("/customers/{name}", name="customers" , defaults={"name"="Vag"})
+     * @param $name
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function customers()
+    public function customers($name )
     {
         return $this->render('customers.html.twig', [
             'controller_name' => 'CustomerController',
+            'name'=> $name,
+            'lovely_variable'=>'ONly love',
         ]);
     }
+
+
 }
--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 
+use App\Entity\Account;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
@@ -15,7 +18,9 @@ class IndexController extends AbstractController
      * @return \Symfony\Component\HttpFoundation\Response
      */
     public function index(Request $request)
+
     {
+
         $name = $request->query->get('name','default Name');
         return $this->render('index.html.twig',
             [
@@ -24,5 +29,37 @@ class IndexController extends AbstractController
                 'name'=>$name
             ]
         );
+
     }
+    /**
+     * @Route("/save", name="save")
+     */
+    public function save(){
+        $entityManager = $this->getDoctrine()->getManager();
+        $account = new Account();
+        $account->setUsername('asdfsdaf');
+        $account->setPassword('asdfdsf');
+
+         $user = $this->findUserbyName('vagos');
+        $account->setUser($user);
+        $entityManager->persist($account);
+        $entityManager->flush();
+
+        return new Response('Saved a user with the id of '.$user->getId());
+    }
+    public function findUser ($id){
+        $user = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->findBy($id);
+        return $user;
+    }
+
+    public function findUserbyName($name){
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        $user = $repository->findOneBy(['name' => $name]);
+
+        return $user;
+    }
+
+
 }
