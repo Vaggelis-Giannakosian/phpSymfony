18176df91a957eb80c9cee7b10a85aafc6063425
lostredakos <e.giannakosian@gmail.com>
2019-02-02 17:32:44 +0200

    some more modifications

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,18 +2,27 @@
 
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
     /**
      * @Route("/", name="index")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        return $this->render('index.html.twig', [
+        $name = $request->query->get('name','default Name');
+        return $this->render('index.html.twig',
+            [
             'controller_name' => 'IndexController',
-        ]);
+                'lovely_variable'=>'Ta paparia mou einai kitrina',
+                'name'=>$name
+            ]
+        );
     }
 }
